d0779184b101c2530db6e082337b38778a100fed
Tomás Aranda <tomasdeveloperfrontend@gmail.com>
2022-04-10 23:55:07 -0300

    add demos explicit

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../css/song.css';
 import { AiTwotoneLike, AiTwotoneDislike, AiOutlineLike, AiOutlineDislike } from 'react-icons/ai';
+import { MdExplicit } from 'react-icons/md';
 
 const Song = ({ item, playSongDemo }) => {
-	const { song, time, like, dislike, url, img } = item;
+	const { song, time, like, dislike, url, img, explicit } = item;
 
 	return (
 		<div className="song" data-song={song} onClick={() => playSongDemo(song, url)}>
 			<img src={img} className="song__img" />
 			<span className="song__name">{song}</span>
+			{explicit ? <MdExplicit className="song__explicit" /> : null}
 			<div className="song__container-like">
 				{like ? <AiTwotoneLike className="song__like" /> : <AiOutlineLike className="song__like" />}
 				{dislike ? (
